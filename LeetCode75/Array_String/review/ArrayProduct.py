11f7aef5d7fe1dc54a769dd1a916f7225fa637f8
Akbermet-Toktobekova <150558178+Akbermet-Toktobekova@users.noreply.github.com>
2025-03-06 19:19:14 +0100

    resolving the ArrayProduct task

---
+++ LeetCode75/Array_String/review/ArrayProduct.py
@@ -0,0 +1,8 @@
+# Input: nums = [1,2,3,4]
+# Output: [24,12,8,6]
+import math
+
+nums = [1,2,3,4]
+#divide the array to 2 each time and get the prod of the second arr containting everything exept the current el// memory and time are baad((
+res = math.prod(nums,)
+print (res)
