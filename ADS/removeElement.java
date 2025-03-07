71f7ca1b256c1a7fc10a18637f4ed241a82874b7
Akbermet-Toktobekova <150558178+Akbermet-Toktobekova@users.noreply.github.com>
2025-03-07 22:06:23 +0100

    Update removeElement.java

--- ADS/removeElement.java
+++ ADS/removeElement.java
@@ -1,15 +1,8 @@
-class Solution {
-    public int removeElement(int[] nums, int val) {
-        int i = 0;
-        int n = nums.length;
-        while (i < n) {
-            if (nums[i] == val) {
-                nums[i] = nums[n - 1];
-                n--;
-            } else {
-                i++;
-            }
-        }
-        return n;
-    }
-}
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0  
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
