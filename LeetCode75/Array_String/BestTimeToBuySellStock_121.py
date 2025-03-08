bb0ce33a08ea5ddcc1a9aff226c43d7ec3190d8f
Akbermet-Toktobekova <150558178+Akbermet-Toktobekova@users.noreply.github.com>
2025-03-08 23:59:44 +0100

    Update BestTimeToBuySellStock_121.py

--- LeetCode75/Array_String/BestTimeToBuySellStock_121.py
+++ LeetCode75/Array_String/BestTimeToBuySellStock_121.py
@@ -1,12 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l, min, res = len(prices), prices[0], 0
+        l, min, result = len(prices), prices[0], 0
 
         for i in range(1,l):
             if (min > prices[i]) :
                 min = prices[i]
-            res = max(res, prices[i] - min)
-        return res, int
+            result = max(res, prices[i] - min)
+        return result
 
 
         
